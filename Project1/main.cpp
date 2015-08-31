020daecd71920c30cc0ba53f158697f475d96f12
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-08-31 13:09:50 +0200

    Added initial files.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,12 +1,52 @@
-// Project 1
+// PROJECT 1
+// Set of linear equations
 
 #include <iostream>
+#include <time.h>
+#include <math.h>
+#include <armadillo>
 
 using namespace std;
+//using namespace arma;
 
+// Functions
+double u(double x)
+{
+    return 1 - (1 - exp(-10))*x - exp(-10*x);
+}
+
+double f(double x)
+{
+    return 100*exp(-10*x);
+}
+
+
+// Main program
 int main()
 {
-    cout << "Hello World!" << endl;
+    clock_t start, finish; // declare start and final time
+    start = clock();
+
+    // START CODE
+    int n;
+    double h = 1./(n+1);
+
+    // GAUSSIAN
+    // …
+
+    // LU DECOMP
+    // …
+
+    // ERROR
+    // …
+
+
+    // END CODE
+
+
+    finish = clock(); // final time
+    cout << ((finish - start)/CLOCKS_PER_SEC) << " seconds"; // print elapsed time
+
     return 0;
 }
 
