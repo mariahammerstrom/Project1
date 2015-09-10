6f91f45324f4922dd9be0211b76f1ce051d79e6f
mariahammerstrom <maria.hammerstrom@astro.uio.no>
2015-09-10 14:29:16 +0200

    Final program.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -31,10 +31,22 @@ int main()
     // START CODE
     int n,i,j;
     n = 10;
-    double h = 1./(n+1);
+    double h = 1./(n+1);            // step length
     cout << "n = " << n << endl;
 
-    // Define a, b, c
+
+    // FLOPS
+    double FLOPS_tridiagonal = 8*n;
+    double FLOPS_Gaussian = (3./2)*n*n*n + n*n;
+    double FLOPS_LUdecomp = n*n*n;
+
+    cout << endl << "FLOPS" << endl;
+    cout << "Tridiagonal: " << "\t" << FLOPS_tridiagonal << endl;
+    cout << "Gaussian: " << "\t" << FLOPS_Gaussian << endl;
+    cout << "LU decomp: " << "\t" << FLOPS_LUdecomp << endl;
+
+
+    // Define vectors a, b, c
     double a[n],b[n],c[n];
     a[0] = 0;
     b[0] = 2;
@@ -55,7 +67,7 @@ int main()
         x[i] = i*h;
         u_analytic[i] = u_solution(x[i]);
         B[i] = h*h*f_func(x[i]);
-        file_analytic << x[i] << "\t" << u_analytic[i] << endl;
+        file_analytic << x[i] << "\t" << u_analytic[i] << endl; // Write solution to file
     }
     file_analytic.close();
 
@@ -64,22 +76,22 @@ int main()
     ofstream file_simple("Project1_simple_algorithm_n" + to_string(n) + ".txt");
 
     // Forward substitution:
-    double u[n],temp[n];
-    u[0] = 0;
+    double v[n],temp[n];
+    v[0] = 0;
 
-    double btemp = b[1];
-    u[1] = B[1]/btemp;
+    double di = b[1];
+    v[1] = B[1]/di;
 
     for(i=2 ; i <= n-1 ; i++){
-        temp[i] = c[i-1]/btemp;
-        btemp = b[i]-a[i]*temp[i];
-        u[i] = (B[i] - a[i]*u[i-1])/btemp;
+        temp[i] = c[i-1]/di;
+        di = b[i]-a[i]*temp[i];
+        v[i] = (B[i] - a[i]*v[i-1])/di;
     }
 
     // Backward substitution:
     for(i=n-1 ; i >= 0 ; i--){
-        u[i] -= temp[i+1]*u[i+1];
-        file_simple << x[i] << "\t" << u[i] << endl;
+        v[i] -= temp[i+1]*v[i+1];
+        file_simple << x[i] << "\t" << v[i] << endl; // Write solution to file
     }
 
     file_simple.close();
@@ -91,7 +103,7 @@ int main()
     // ERROR
     double e[n], err[n], max=0.0;
     for(i=1 ; i < n ; i++){
-      e[i] = (u[n-i]-u_analytic[i])/u_analytic[i];
+      e[i] = (v[n-i]-u_analytic[i])/u_analytic[i];
       err[i] = log10(fabs(e[i]));
       if (err[i]>max)
           max = fabs(e[i]);
@@ -104,6 +116,7 @@ int main()
     clock_t start_LU, finish_LU; // declare start and final time, LU decomposition
     start_LU = clock();
 
+    // Set up matrix A
     double ** A;
     A = new double*[n];
     for (i = 0; i < n; i++)
@@ -120,17 +133,15 @@ int main()
         }
     }
 
-    // LU decomp: use lib.cpp function: void ludcmp(double **a, int n, int *indx, double *d)
-    // Solve linear equation using LU decomposition with function: void lubksb(double **a, int n, int *indx, double *b)
-
     double d;
     int *indx = new int[n];
     ludcmp(A, n, indx, &d); // changes A to LU decomposition
-    lubksb(A, n, indx, B); // is this correct? How to make a plot?
+    lubksb(A, n, indx, B); // solves the linear equations using the LU decomposition
 
     finish_LU = clock(); // final time
     cout << "Time (LU decomp):" << "\t" << ((finish_LU - start_LU)/CLOCKS_PER_SEC) << " seconds" << endl; // print elapsed time
 
+    // Write solution to file
     ofstream file_LUdecomp("Project1_LU_decomp_n" + to_string(n) + ".txt");
 
     for(i=0 ; i<n ; i++){
@@ -139,23 +150,10 @@ int main()
 
     file_LUdecomp.close();
 
-
+    // Clear memory
      for (i=0; i<n ; i++)
          delete [] A[i];
      delete [] A;
-
-
-     // FLOPS
-     // Double check these formulas! Also, need proportionality constants.
-
-     double FLOPS_tridiagonal = 8*n;
-     double FLOPS_Gaussian = (3./2)*n*n*n + n*n;
-     double FLOPS_LUdecomp = n*n*n;
-
-     cout << endl << "FLOPS" << endl;
-     cout << "Tridiagonal: " << "\t" << FLOPS_tridiagonal << endl;
-     cout << "Gaussian: " << "\t" << FLOPS_Gaussian << endl;
-     cout << "LU decomp: " << "\t" << FLOPS_LUdecomp << endl;
 
     return 0;
 }
